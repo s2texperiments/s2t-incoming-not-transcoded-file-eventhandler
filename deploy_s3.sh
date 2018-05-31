a3e924977e865b19415e39bd28176bf7375046cc
Stefan Breitenstein <stefan.breitenstein@gmail.com>
2018-06-01 00:07:55 +0200

    add travis support

--- deploy_s3.sh
+++ deploy_s3.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
+cp index.js test/index.js
+
 #mkdir deploy
 #chmod 777 deploy
 #zip -r deploy/s2t-incoming-not-transcoded-file-eventhandler.zip index.js node_modules/
-cp index.js test/index.js
