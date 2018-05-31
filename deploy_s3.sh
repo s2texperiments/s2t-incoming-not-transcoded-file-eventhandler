e5da394727fd4268a3366db82d004e69edf7a8ca
Stefan Breitenstein <stefan.breitenstein@gmail.com>
2018-06-01 00:30:16 +0200

    add missing s3Api.js

--- deploy_s3.sh
+++ deploy_s3.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir deploy
-zip -r deploy/s2t-incoming-not-transcoded-file-eventhandler.zip index.js node_modules/
+zip -r deploy/s2t-incoming-not-transcoded-file-eventhandler.zip index.js s3Api.js node_modules/
