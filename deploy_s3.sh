90952dea54bf249c9d2c7c27eca1d73e3cc5bb11
ste-xx <stefan.breitenstein@gmail.com>
2018-07-04 19:09:47 +0200

    [rename zip to current git repo name]

--- deploy_s3.sh
+++ deploy_s3.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir deploy
-zip -r deploy/s2t-incoming-not-transcoded-file-eventhandler.zip index.js s3Api.js snsApi.js node_modules/
+zip -r deploy/s2t-s3-eventhandler.zip index.js s3Api.js snsApi.js node_modules/
